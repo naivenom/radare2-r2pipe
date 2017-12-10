85e4862290d98d49ae648c1d9024f2f1875f6dcc
naivenom <naivenom@protonmail.com>
2017-12-10 12:34:07 +0100

    Update __init__.py
    The latest r2pipe module fails with python3.6 on the 2nd command with r2pipe.cmdj.Error: I/O operation on closed file
    The cause of the error seems to be the TextIOWrapper being created on every call in:
    r = TextIOWrapper(self.process.stdout, encoding='utf8') 
    This fixed:
    r = getattr(self, '_process_stdout_wrapper', None)
                            if r is None:
                              r = TextIOWrapper(self.process.stdout, encoding='utf8')
                              self._process_stdout_wrapper = r

--- python/r2pipe/__init__.py
+++ python/r2pipe/__init__.py
@@ -167,6 +167,10 @@ class open:
                         self.process.stdin.write(bytes(cmd + '\n', 'utf-8'))
                         # XXX: Use the TextIOWrapper or we can get stuck in an endless loop!
                         r = TextIOWrapper(self.process.stdout, encoding='utf8')
+                        r = getattr(self, '_process_stdout_wrapper', None)
+                        if r is None:
+                          r = TextIOWrapper(self.process.stdout, encoding='utf8')
+                          self._process_stdout_wrapper = r
                 else:
                         self.process.stdin.write(cmd + '\n')
                         r = self.process.stdout
